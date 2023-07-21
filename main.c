f6a844e783559c6b13c2e3b9f3c65ec1334353e1
BhuvanShridhar <116069041+BhuvanShridhar@users.noreply.github.com>
2023-07-21 12:01:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,51 @@
 #include<stdio.h>
-int main()
+
+void pick();
+
+void main() 
+{ int player1,player2;
+ 
+ printf("enter 1 for Scissors\n");
+ printf("enter 2 for Rock\n"); 
+ printf("enter 3 for paper\n");
+
+ pick();
+
+} 
+void pick() 
 {
-    printf("hello Vikas");
+
+  int player1,player2;
+    
+  printf("player 1 it is your turn\n "); scanf("%d",&player1);
+  printf("player 2 now it is your turn\n"); scanf("%d",&player2);
+
+ if(player1==player2)
+
+ printf("it is a draw");
+      
+ else if(player1==1&&player2==2) 
+      { 
+          printf("player 2 wins");
+      } 
+ else if(player1==2&&player2==1) 
+      { 
+          printf("player 1 wins");
+      }
+ else if(player1==3&&player2==2)
+      { 
+          printf("player 1 wins"); 
+      } 
+ else if(player1==2&&player2==3) 
+      {
+          printf("player 1 wins");
+      } 
+ else if(player1==1&&player2==3)
+      {
+          printf("player 1 wins"); 
+      } 
+ else {  
+       printf("player 2 wins");
+      }
+}
 }
